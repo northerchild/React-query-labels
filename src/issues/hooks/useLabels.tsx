68ec49721064c8f2b1ba09149c02e1edab9fc9ff
jhonata ospina <osarus-13@hotmail.com>
2023-05-04 11:28:50 -0500

    primera parte de los labels

---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,38 @@
+import { githubApi } from '../../api/githubApi';
+import { Labels } from '../interfaces/label';
+import {useQuery} from '@tanstack/react-query'
+
+const getLabels = async():Promise<Labels[]> => {
+    const {data } = await githubApi.get<Labels[]>('/labels')
+    return data
+  }
+
+
+export const useLabels = ()=> {
+    const labelsQuery = useQuery(
+        ['labels'],
+        getLabels,{
+            //staleTime: 1000 * 60 *60,
+            placeholderData:[{
+              id: 791921801,
+              node_id: "MDU6TGFiZWw3OTE5MjE4MDE=",
+              url: "https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F",
+              name: "❤️",
+              color: "ffffff",
+              default: false,
+             
+            },
+            {
+              id: 69105383,
+              node_id: "MDU6TGFiZWw2OTEwNTM4Mw==",
+              url: "https://api.github.com/repos/facebook/react/labels/Browser:%20IE",
+              name: "Browser: IE",
+              color: "c7def8",
+              default: false,
+            }
+          ]
+        }
+      )
+    
+      return labelsQuery
+}
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+import { router } from './router';
+
+//! Remover enable css source maps
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <RouterProvider router={ router } />
+    </QueryClientProvider>
+  </React.StrictMode>
+)
